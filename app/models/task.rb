a9fcc62fe240aad7349abe211112a3819f03e0f0
nodmentor2ygn <nodmentor2ygn@gmail.com>
2020-10-25 14:40:57 +0630

    Association settings

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,4 +8,5 @@ class Task < ApplicationRecord
   enum priority: {高:0, 中:1, 低:2}
   scope :title_search, -> (title) {where("title LIKE ?", "%#{title}%")}
   scope :status_search,  -> (status) {where(status: status)}
+  belongs_to :user
 end
